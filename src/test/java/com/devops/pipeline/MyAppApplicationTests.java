a1e7e7f471421d9f09da42ec6d1b10471156bb17
HeeDon Kang <105708291+OilWork@users.noreply.github.com>
2022-12-21 00:18:33 +0900

    Add files via upload

---
+++ src/test/java/com/devops/pipeline/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
